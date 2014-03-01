cd0da131540d446d517716b24e42b0b0abf26c38
avinashega <avinash.ega@gmail.com>
2014-03-01 19:53:03 +0530

    post pings to rabbitmq and config changes

---
+++ app/jobs/ping.js
@@ -0,0 +1,34 @@
+var i = require('../i');
+var Q = require('q');
+var agendaJobs = i.db().agendaJobs;
+var config = require('getconfig');
+var open = require('amqplib').connect(config.amqp.RABBITMQ_TX_URL);
+var ObjectID = require('mongoskin').ObjectID;
+Q.longStackSupport = true;
+
+
+_setJobIsPinging = function(id, status) {
+    Q.nbind(agendaJobs.update, agendaJobs) (
+        {
+            _id: ObjectID.createFromHexString(id)
+        },
+        {
+            $set: {isPinging: status}
+        }
+    );
+};
+
+module.exports = function (agenda) {
+    agenda.define('ping', {concurrency: 1000}, function (job, done) {
+        var jobId = job.attrs._id;
+        console.log("[pingJob] [.] Posting job ID "+jobId);
+        open.then(function(conn) {
+                var ok = conn.createChannel()
+                .then(function(ch) {
+                    ch.sendToQueue('jobs_queue', new Buffer(String(jobId)));
+                });
+                _setJobIsPinging(jobId.toString(), true);
+                done();
+            });
+    });
+};
