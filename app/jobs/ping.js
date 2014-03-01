2eeee77c971e0a05f258a33e5ffaf07837ccd748
avinashega <avinash.ega@gmail.com>
2014-03-02 01:53:36 +0530

    amqp config

--- app/jobs/ping.js
+++ app/jobs/ping.js
@@ -25,7 +25,7 @@ module.exports = function (agenda) {
         open.then(function(conn) {
                 var ok = conn.createChannel()
                 .then(function(ch) {
-                    ch.sendToQueue('jobs_queue', new Buffer(String(jobId)));
+                    ch.sendToQueue(config.amqp.jobs_queue, new Buffer(String(jobId)));
                 });
                 _setJobIsPinging(jobId.toString(), true);
                 done();
