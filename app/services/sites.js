7e97b2b7498b182dd0d3f198ea04a7fcd52e1ebf
avinashega <avinash.ega@gmail.com>
2014-03-04 13:01:42 +0530

    responsiveness

--- app/services/sites.js
+++ app/services/sites.js
@@ -14,11 +14,13 @@ module.exports={
         req.sanitize('frequency').trim();
         req.sanitize('method').trim();
         req.sanitize('path').trim();
+        req.sanitize('responsiveness').trim();
 
         req.assert('url', 'Valid URL required').regex(/^(?=.{1,254}$)((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}$/i);
         if(req.body.port != "")
         	req.assert('port', 'Valid port required').regex(/^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$/);
         req.assert('frequency', 'Frequency should be an integer greater than 0').regex(/^[1-9]\d*$/);
+        req.assert('responsiveness', 'Responsiveness should be an integer greater than 0').regex(/^[1-9]\d*$/);
 		return q.fcall(function () {
             var errors = req.validationErrors();
             if (errors) {
