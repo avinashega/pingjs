77a227ac057376d99ca14573f13d8cabedbf47db
avinashega <avinash.ega@gmail.com>
2014-03-06 21:16:12 +0530

    contact us

--- app/controllers/static.js
+++ app/controllers/static.js
@@ -5,8 +5,12 @@ module.exports={
 		tos: function(req, res){
 			res.render('tos');
 		},
+		contact: function(req, res){
+			res.render('contact');
+		},
 		routes: function(app){
 			app.get('/privacy', this.privacy);
 			app.get('/tos', this.tos);
+			app.get('/contact', this.contact);
 		}
 }
