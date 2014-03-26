bd4ea7d9b92912dc03ab5c9e508886903ea6e748
avinashega <avinash.ega@gmail.com>
2014-03-26 14:29:01 +0530

    rabbitmq cleanup

--- app/jobs/ping.js
+++ app/jobs/ping.js
@@ -23,12 +23,13 @@ module.exports = function (agenda) {
         var jobId = job.attrs._id;
         console.log("[pingJob] [.] Posting job ID "+jobId);
         open.then(function(conn) {
-                var ok = conn.createChannel()
+                return conn.createChannel()
                 .then(function(ch) {
+                	ch.assertQueue(config.amqp.job_queue);
                     ch.sendToQueue(config.amqp.job_queue, new Buffer(String(jobId)));
+                    _setJobIsPinging(jobId.toString(), true);
+                    done();
                 });
-                _setJobIsPinging(jobId.toString(), true);
-                done();
-            });
+            }).then(null, console.warn);
     });
 };
